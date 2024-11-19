da7f1751f80dc4bcb9caafcc8d3d2dd67f1b2f6b
Rayane le Goat <rayane.ould.herault@gmail.com>
2024-11-19 15:26:53 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,39 @@
-#include <iostream>
+void main(void) {
+    int pause;
 
-int main()
-{
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    CLpoint* p1;
+    CLpoint* p2;
+    CLpoint* p3;
+    CLparcours* parcours;
+
+    p1 = new CLpoint(0.0, 0.0);
+    p2 = new CLpoint(1.0, 1.0);
+    p3 = new CLpoint(2.0, 2.0);
+    parcours = new CLparcours2D(3);
+
+    parcours->ajouterPoint(p1);
+    parcours->ajouterPoint(p2);
+    parcours->ajouterPoint(p3);
+
+    cout << parcours->calculDistance() << endl;
+    parcours->message();
+
+    p1 = new CLpoint3D(0.0, 0.0, 0.0);
+    p2 = new CLpoint3D(1.0, 1.0, 1.0);
+    p3 = new CLpoint3D(2.0, 2.0, 2.0);
+    parcours = new CLparcours3D(3);
+
+    parcours->ajouterPoint(p1);
+    parcours->ajouterPoint(p2);
+    parcours->ajouterPoint(p3);
+
+    cout << parcours->calculDistance() << endl;
+    parcours->message();
+
+    p1 = new CLpoint(0.0, 0.0);
+    p2 = new CLpoint(1.0, 1.0);
+
+    (p1 = p2)->afficherCoordo();
+
+    cin >> pause;
 }
